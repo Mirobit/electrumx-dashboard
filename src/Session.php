35408d8d48b36ac61bad5194a4d8b026d4ad6973
Michael <13236924+Mirobit@users.noreply.github.com>
2020-11-28 12:52:00 +0100

    Fix spacing

--- src/Session.php
+++ src/Session.php
@@ -5,14 +5,14 @@ namespace App;
 class Session{	
 	public $id; // int
 	public $ip; // string
-    public $ipv6; // bool
-    public $flag; // string
-    public $ssl; // bool
+	public $ipv6; // bool
+	public $flag; // string
+	public $ssl; // bool
 	public $trafficOut; // int (KB)
 	public $trafficIn; // int (KB)
 	public $trafficTotal; // int (KB)
-    public $messagesRec; // int
-    public $messagesSen; // int
+	public $messagesRec; // int
+	public $messagesSen; // int
 	public $ageHours; // int (hours)
 	public $age; // int (seconds)
 	public $client; // int
@@ -27,26 +27,26 @@ class Session{
 	
 	function __construct($session) {
 		$this->id = checkInt($session[0]);
-        $this->flag = htmlspecialchars($session[1]);
-        if(substr($this->flag, 0, 1) == "S"){
-            $this->ssl = TRUE;
-        }else{
-            $this->ssl = FALSE;
-        }
+			$this->flag = htmlspecialchars($session[1]);
+				if(substr($this->flag, 0, 1) == "S"){
+					$this->ssl = TRUE;
+				}else{
+					$this->ssl = FALSE;
+				}
 		$this->ip = getCleanIP($session[2]);
 		$this->ipv6 = checkIfIpv6($this->ip);
-        $this->client = htmlspecialchars(($session[3]));
+		$this->client = htmlspecialchars(($session[3]));
 		$this->protocol = htmlspecialchars(($session[4]));
 		$this->requestsC = checkInt($session[5]);
-        $this->txsC = checkInt($session[6]);
-        $this->subscriptionsC = checkInt($session[7]);
-        $this->messagesRec = checkInt($session[8]);
-        $this->trafficIn = bytesToMb($session[9]);
-        $this->messagesSen = checkInt($session[10]);
-        $this->trafficOut = bytesToMb($session[11]);
-        $this->traffic = $this->trafficOut + $this->trafficIn;
-        $this->age = round(checkInt($session[12]));
-        $this->ageHours = round($this->age/3600,1);
+		$this->txsC = checkInt($session[6]);
+		$this->subscriptionsC = checkInt($session[7]);
+		$this->messagesRec = checkInt($session[8]);
+		$this->trafficIn = bytesToMb($session[9]);
+		$this->messagesSen = checkInt($session[10]);
+		$this->trafficOut = bytesToMb($session[11]);
+		$this->traffic = $this->trafficOut + $this->trafficIn;
+		$this->age = round(checkInt($session[12]));
+		$this->ageHours = round($this->age/3600,1);
 	}			
 }
 ?>
