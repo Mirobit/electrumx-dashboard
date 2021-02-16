eb77d96ddc2d30d6c45278a04fb39497d656b74e
Michael <13236924+Mirobit@users.noreply.github.com>
2021-02-16 23:02:26 +0100

    Add docker instructions

--- src/Config.sample.php
+++ src/Config.sample.php
@@ -7,15 +7,24 @@
       // EXD folder to something unique, if accessible via the web.
       // Leave empty to disable it (only do this on a local setup that can't be reached from the outside).
       const PASSWORD = "MY-PASSWORD";
-      // IP that can access EXD:
-      // If empty (""), any IP can access EXD.
-      // If "localhost" only localhost (IPv4/v6) can access EXD).
-      // If specific IP (e.g. "84.12.32.297"), localhost and the specific IP can access EXD.
+      
+      /* 
+      IP that can access EXD:
+        "": any IP can access EXD
+        "localhost": only localhost (IPv4/v6) can access EXD)
+        "84.12.32.297": localhost and the specific IP (e.g. 84.12.32.297) can access EXD
+      */
       const ACCESS_IP = "";
 
-      // IP of ElectrumX RPC server
+      /*
+      IP of the ElextrumX RPC Server, usually localhost.
+      DOCKER:
+      -> Windows/Mac: host.docker.internal
+      -> Linux: 172.17.0.1 (Don't forget to open the port since docker uses by default a different network interface: 
+        'sudo ufw allow in on docker0 from 172.17.0.0/16 to any port 8000' -> Replace 8000 with the ElextrumX RPC port)
+      */
       const RPC_IP = "127.0.0.1";
-      // Port of ElectrumX RPC server
+      // Port of the ElectrumX RPC server
       const RPC_PORT = "8000";
 
       // EXD uses ip-api.com to get the country and isp of peers/sessions. The API is limited to 15 
